a6711968b95259e07bd17c3ab5fde81f83ecd249
59114_Chetana <deslechetna1999@gmail.com>
2022-04-12 21:48:57 +0530

    [chetana] Added : flip coin programme

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class FlipCoin
+    {
+        const double IS_TAIL = 0.5;
+        int headCount = 0, tailCount = 0;
+        public void Flip()
+        {
+            Console.WriteLine("Enter number of flips");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                while (num > 0)
+                {
+                    Random random = new Random();
+                    double option = random.NextDouble();
+                    if (option < IS_TAIL)
+                    {
+                        tailCount++;
+                    }
+                    else
+                    {
+                        headCount++;
+                    }
+                    num--;
+                }
+            }
+
+            Console.WriteLine("tailCount : " + tailCount);
+            Console.WriteLine("headCount : " + headCount);
+            double HeadCountPercentage = headCount % 100;
+            double tailCountPercentage = tailCount % 100;
+            Console.WriteLine("Percentage of the HEAD :" + " " + HeadCountPercentage);
+            Console.WriteLine("percentage of the TAIL :" + " " + tailCountPercentage);
+
+        }
+    }
+}
+
+
+
+
+
+
+
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace BasicPrograms;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Welcome to Basic Programs");
+        bool end = true;
+        Console.WriteLine("1. FlipCoin\n 2. End the Program");
+        while (end == true)
+        {
+            Console.WriteLine("Take an option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.Flip();
+                    break;
+                case 2:
+                    end = false;
+                    break;
+                default:
+                    Console.WriteLine("Enter Proper Option To Execute");
+                    break;
+            }
+        }
+    }
+}
