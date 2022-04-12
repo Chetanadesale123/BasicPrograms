e77a21a55c9b769037d99621795b60f02b043d41
59114_Chetana <deslechetna1999@gmail.com>
2022-04-12 23:21:37 +0530

    [chetana] Added :Checking of no is even or odd no

---
+++ BasicPrograms/CheckEvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class CheckEvenOrOdd
+    {
+        public void EvenOrOdd()
+        {
+            int num;
+            Console.Write("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is an Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Entered Number is an Odd Number");
+            }
+        }
+    }
+}
+
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -7,13 +7,14 @@ class Program
     {
         Console.WriteLine("Welcome to Basic Programs");
         bool end = true;
-        Console.WriteLine("1. FlipCoin\n  2. Leap year \n 3.Power Of Two \n 4.Harmonic no \n 5.Prime Factors \n 6.Compute Quotient and Remainder \n 7.Swap Two no \n 8. End the Program");
+        Console.WriteLine("1. FlipCoin\n  2. Leap year \n 3.Power Of Two \n 4.Harmonic no \n 5.Prime Factors \n 6.Compute Quotient and Remainder \n 7.Swap Two no \n 8. Check Even Or Odd no\n 9. End the Program");
         while (end == true)
         {
             Console.WriteLine("Take an option to execute");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
+                
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.Flip();
@@ -43,6 +44,10 @@ class Program
                    swapTwoNo.SwapNo();
                     break;
                 case 8:
+                    CheckEvenOrOdd EvenOrOdd = new CheckEvenOrOdd();
+                    EvenOrOdd.EvenOrOdd();
+                    break;
+                case 9:
                     end = false;
                     break;
                 default:
