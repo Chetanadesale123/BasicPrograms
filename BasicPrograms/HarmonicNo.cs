19bfaa60c48d2efbf83e8009050f1bd209a0275f
59114_Chetana <deslechetna1999@gmail.com>
2022-04-12 22:20:08 +0530

    [chetana] Added : Harmonic number programme

---
+++ BasicPrograms/HarmonicNo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class HarmonicNo
+    {
+        public void harmonicNumber()
+        {
+            double harmonic = 0.0;
+
+            Console.WriteLine("enter the Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                harmonic += 1 / (double)i;
+            }
+            Console.WriteLine("Harmonic No Series:" + harmonic);
+        }
+    }
+}
+
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         Console.WriteLine("Welcome to Basic Programs");
         bool end = true;
-        Console.WriteLine("1. FlipCoin\n  2. Leap year \n 3.Power Of Two \n  4. End the Program");
+        Console.WriteLine("1. FlipCoin\n  2. Leap year \n 3.Power Of Two \n 4.Harmonic no \n 5. End the Program");
         while (end == true)
         {
             Console.WriteLine("Take an option to execute");
@@ -27,6 +27,10 @@ class Program
                     power.Power();
                     break;
                 case 4:
+                    HarmonicNo harmonicNo = new HarmonicNo();
+                    harmonicNo.harmonicNumber();
+                    break;
+                case 5:
                     end = false;
                     break;
                 default:
