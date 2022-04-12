27d6f205fe30285d6088d8c2cb47399a3c0adc6e
59114_Chetana <deslechetna1999@gmail.com>
2022-04-12 22:45:41 +0530

    [chetana] Added : prime factors programme

---
+++ BasicPrograms/PrimeFactors.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class PrimeFactors
+    {
+        public void PrimeFactor()
+        {
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    int Count = 0;
+                    while (n % i == 0)
+                    {
+                        n = n / i;
+                        Count++;
+                    }
+                    Console.WriteLine(i + " " + "^" + Count);
+                }
+            }
+
+
+        }
+    }
+}
+
+
